d4d1366e1db0d4f744742df9bb7342681622e078
Plenyx <Plenyx@users.noreply.github.com>
2019-06-27 23:49:36 +0200

    Release 45

--- Forms/FormMain.cs
+++ Forms/FormMain.cs
@@ -48,7 +48,7 @@ namespace PlenBotLogUploader
 
         // constants
         private const int minFileSize = 12288;
-        private const int uploaderRelease = 44;
+        private const int uploaderRelease = 45;
         #endregion
 
         #region constructor
