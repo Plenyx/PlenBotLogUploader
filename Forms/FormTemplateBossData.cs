956414fdf5bb9023197825f625adc17650d7ac1d
Plenyx <Plenyx@users.noreply.github.com>
2019-07-18 20:51:48 +0200

    Added confirmation for the templates

--- Forms/FormTemplateBossData.cs
+++ Forms/FormTemplateBossData.cs
@@ -27,29 +27,39 @@ namespace PlenBotLogUploader
 
         private void ButtonSuccessSave_Click(object sender, EventArgs e)
         {
-            var BossesToChange = bossDataLink.AllBosses
-                .Where(anon => !Bosses.IsGolem(anon.Value.BossId))
-                .Where(anon => !Bosses.IsWvW(anon.Value.BossId))
-                .Where(anon => !Bosses.IsEvent(anon.Value.BossId))
-                .ToDictionary(anon => anon.Key, anon => anon.Value);
-            foreach(var key in BossesToChange.Keys)
+            DialogResult result = MessageBox.Show($"This will change all non-golem, non-wvw, non-event Twitch message on success to \"{textBoxSuccessMessage.Text}\".\nAre you sure?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result.Equals(DialogResult.Yes))
             {
-                var boss = bossDataLink.AllBosses[key];
-                boss.SuccessMsg = textBoxSuccessMessage.Text;
+                var BossesToChange = bossDataLink.AllBosses
+                    .Where(anon => !Bosses.IsGolem(anon.Value.BossId))
+                    .Where(anon => !Bosses.IsWvW(anon.Value.BossId))
+                    .Where(anon => !Bosses.IsEvent(anon.Value.BossId))
+                    .ToDictionary(anon => anon.Key, anon => anon.Value);
+                foreach (var key in BossesToChange.Keys)
+                {
+                    var boss = bossDataLink.AllBosses[key];
+                    boss.SuccessMsg = textBoxSuccessMessage.Text;
+                }
+                MessageBox.Show("All changes are saved.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
         private void ButtonFailSave_Click(object sender, EventArgs e)
         {
-            var BossesToChange = bossDataLink.AllBosses
-                .Where(anon => !Bosses.IsGolem(anon.Value.BossId))
-                .Where(anon => !Bosses.IsWvW(anon.Value.BossId))
-                .Where(anon => !Bosses.IsEvent(anon.Value.BossId))
-                .ToDictionary(anon => anon.Key, anon => anon.Value);
-            foreach (var key in BossesToChange.Keys)
+            DialogResult result = MessageBox.Show($"This will change all non-golem, non-wvw, non-event Twitch message on fail to \"{textBoxFailMessage.Text}\".\nAre you sure?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result.Equals(DialogResult.Yes))
             {
-                var boss = bossDataLink.AllBosses[key];
-                boss.FailMsg = textBoxFailMessage.Text;
+                var BossesToChange = bossDataLink.AllBosses
+                    .Where(anon => !Bosses.IsGolem(anon.Value.BossId))
+                    .Where(anon => !Bosses.IsWvW(anon.Value.BossId))
+                    .Where(anon => !Bosses.IsEvent(anon.Value.BossId))
+                    .ToDictionary(anon => anon.Key, anon => anon.Value);
+                foreach (var key in BossesToChange.Keys)
+                {
+                    var boss = bossDataLink.AllBosses[key];
+                    boss.FailMsg = textBoxFailMessage.Text;
+                }
+                MessageBox.Show("All changes are saved.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
     }
--- UnitTestsForPlenBotLogUploader/PingTests.cs
+++ UnitTestsForPlenBotLogUploader/PingTests.cs
@@ -7,7 +7,7 @@ namespace UnitTestsForPlenBotLogUploader
     public class PingTests
     {
         [TestMethod]
-        public void TestGetNonAuth()
+        public void TestGetNonAuthSuccess()
         {
             PingConfiguration config = new PingConfiguration()
             {
