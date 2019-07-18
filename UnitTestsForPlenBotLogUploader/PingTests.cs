60e4d854896506c91cd780f9c3ad2b47d4e7f04b
Plenyx <Plenyx@users.noreply.github.com>
2019-07-18 20:43:34 +0200

    Added ping test to test get

---
+++ UnitTestsForPlenBotLogUploader/PingTests.cs
@@ -0,0 +1,29 @@
+﻿using PlenBotLogUploader.RemotePing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestsForPlenBotLogUploader
+{
+    [TestClass]
+    public class PingTests
+    {
+        [TestMethod]
+        public void TestGetNonAuth()
+        {
+            PingConfiguration config = new PingConfiguration()
+            {
+                Active = true,
+                Name = "Test ping",
+                Method = PingMethod.Get,
+                Authentication = new PingAuthentication()
+                {
+                    Active = true,
+                    AuthName = "sign",
+                    AuthToken = "testsign",
+                    UseAsAuth = false
+                },
+                URL = "https://plenbot.net/uploader/ping/"
+            };
+            Assert.IsTrue(config.TestPingAsync().Result.Success);
+        }
+    }
+}
