f18945d9acdc4a3ac9ce54cea5b0272f3d9460d0
Plenyx <Plenyx@users.noreply.github.com>
2019-09-29 18:49:22 +0200

    Using png over ico, Discord doesn't like it

--- DiscordAPI/DiscordAPIJSONContentEmbedFooter.cs
+++ DiscordAPI/DiscordAPIJSONContentEmbedFooter.cs
@@ -3,6 +3,6 @@
     public class DiscordAPIJSONContentEmbedFooter
     {
         public string Text { get; set; } = $"PlenBot Log Uploader release {Properties.Settings.Default.ReleaseVersion}";
-        public string Icon_url { get; set; } = "https://plenbot.net/uploader/img/favicon.ico";
+        public string Icon_url { get; set; } = "https://plenbot.net/uploader/img/favicon.png";
     }
 }
