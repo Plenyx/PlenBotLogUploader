d472a3f2338cf670b6f6862925689ffb039f1ac6
Plenyx <Plenyx@users.noreply.github.com>
2019-11-22 17:09:54 +0100

    Minor code changes

--- DiscordAPI/DiscordWebhookData.cs
+++ DiscordAPI/DiscordWebhookData.cs
@@ -5,10 +5,25 @@ namespace PlenBotLogUploader.DiscordAPI
 {
     public class DiscordWebhookData
     {
+        /// <summary>
+        /// Tells if the webhook is currently active
+        /// </summary>
         public bool Active { get; set; } = false;
+        /// <summary>
+        /// Name of the webhook
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// URL for the webhook
+        /// </summary>
         public string URL { get; set; }
+        /// <summary>
+        /// Tells if the webhook is executed only if the ecounter is a success
+        /// </summary>
         public bool OnlySuccess { get; set; } = false;
+        /// <summary>
+        /// Tells if players are showed in the webhook
+        /// </summary>
         public bool ShowPlayers { get; set; } = true;
 
         public async Task<bool> TestWebhookAsync(FormMain mainLink)
