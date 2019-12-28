06d4c9ba262ae9cd5eda6c457ca1388223c35c3f
Plenyx <Plenyx@users.noreply.github.com>
2019-12-29 00:56:02 +0100

    Fix an error in the updates

--- Tools/CompatibilityUpdate.cs
+++ Tools/CompatibilityUpdate.cs
@@ -33,14 +33,14 @@ namespace PlenBotLogUploader.Tools
                             bosses.Add(bosses.Count + 1, new DPSReport.BossData() { BossId = bossId, Name = values[1], SuccessMsg = values[2], FailMsg = values[3], Icon = values[4], Type = (DPSReport.BossType)(type), Event = (isEvent == 1) ? true : false });
                         }
                     }
+                    if (bosses.Where(anon => anon.Value.BossId.Equals(21333)).Count() == 0)
+                    {
+                        File.AppendAllText($@"{localDir}\boss_data.txt", "21333<;>Freezie<;><boss> kill: <log><;><boss> pull: <log><;>https://dps.report/cache/https_wiki.guildwars2.com_images_thumb_8_8b_Freezie.jpg_189px-Freezie.jpg<;>3<;>0");
+                    }
                 }
                 catch
                 {
                     // do nothing, since the file does not exist, or is corrupted (data does not line up)
-                }
-                if (bosses.Where(anon => anon.Value.BossId.Equals(21333)).Count() == 0)
-                {
-                    File.AppendAllText($@"{localDir}\boss_data.txt", "21333<;>Freezie<;><boss> kill: <log><;><boss> pull: <log><;>https://dps.report/cache/https_wiki.guildwars2.com_images_thumb_8_8b_Freezie.jpg_189px-Freezie.jpg<;>3<;>0");
                 }
             }
             // end of updates
