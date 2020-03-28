4d852cff235b771cfa224354de973a407e453abb
Plenyx <Plenyx@users.noreply.github.com>
2020-03-28 22:54:05 +0100

    Release 57

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace PlenBotLogUploader.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -397,7 +397,7 @@ namespace PlenBotLogUploader.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("56")]
+        [global::System.Configuration.DefaultSettingValueAttribute("57")]
         public int ReleaseVersion {
             get {
                 return ((int)(this["ReleaseVersion"]));
@@ -406,7 +406,7 @@ namespace PlenBotLogUploader.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("56")]
+        [global::System.Configuration.DefaultSettingValueAttribute("57")]
         public int SavedVersion {
             get {
                 return ((int)(this["SavedVersion"]));
