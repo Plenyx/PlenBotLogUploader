34f3371ab547be0ac5fc635ead3be3065b6dd924
Plenyx <Plenyx@users.noreply.github.com>
2020-04-11 00:27:52 +0200

    Fixed a crash when adding a new Discord webhook

--- Forms/FormEditDiscordWebhook.cs
+++ Forms/FormEditDiscordWebhook.cs
@@ -44,7 +44,7 @@ namespace PlenBotLogUploader
                 .ToDictionary(anon => anon.Key, anon => anon.Value);
             foreach (var bossNumber in bosses.Keys)
             {
-                checkedListBoxBossesEnable.Items.Add(new BossesDisableHelperClass() { BossID = bosses[bossNumber].BossId, Text = $"{bosses[bossNumber].Type}: {bosses[bossNumber].Name} ({bosses[bossNumber].BossId})" }, data.IsBossEnabled(bosses[bossNumber].BossId));
+                checkedListBoxBossesEnable.Items.Add(new BossesDisableHelperClass() { BossID = bosses[bossNumber].BossId, Text = $"{bosses[bossNumber].Type}: {bosses[bossNumber].Name} ({bosses[bossNumber].BossId})" }, data?.IsBossEnabled(bosses[bossNumber].BossId) ?? true);
             }
         }
 
