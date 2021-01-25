e719c6168afd9e54fba469142492936e9033fe77
Plenyx <1507236+Plenyx@users.noreply.github.com>
2021-01-25 13:34:41 +0100

    Removed unneeded usings

--- DiscordAPI/DiscordWebhooks.cs
+++ DiscordAPI/DiscordWebhooks.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Collections.Generic;
 
 namespace PlenBotLogUploader.DiscordAPI
