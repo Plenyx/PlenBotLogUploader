e988734d15cadb8ad12e9a359e5f982abdd13bff
Plenyx <1507236+Plenyx@users.noreply.github.com>
2021-02-06 19:05:18 +0100

    Cleanup the old code from the old file
    Also the webhooks now take into consideration, whether you limit webhooks to success/fail only

--- Forms/FormDiscordWebhooks.cs
+++ Forms/FormDiscordWebhooks.cs
@@ -21,9 +21,6 @@ namespace PlenBotLogUploader
         private readonly Dictionary<int, BossData> allBosses = Bosses.GetAllBosses();
         private int webhookIdsKey = 0;
         private readonly Dictionary<int, DiscordWebhookData> allWebhooks = DiscordWebhooks.GetAllWebhooks();
-
-        // consts
-        private const int maxAllowedMessageSize = 1800;
         #endregion
 
         public FormDiscordWebhooks(FormMain mainLink)
@@ -169,242 +166,14 @@ namespace PlenBotLogUploader
 
         public async Task ExecuteSessionWebhooksAsync(List<DPSReportJSON> reportsJSON, LogSessionSettings logSessionSettings)
         {
-            var discordEmbedsSuccessFailure = new List<DiscordAPIJSONContentEmbed>();
-
-            var RaidLogs = reportsJSON
-                .Where(x => Bosses.GetWingForBoss(x.EVTC.BossId) > 0)
-                .Select(x => new { LogData = x, RaidWing = Bosses.GetWingForBoss(x.EVTC.BossId) })
-                .OrderBy(x => x.LogData.UploadTime)
-                .ToList();
-            if (logSessionSettings.SortBy.Equals(LogSessionSortBy.Wing))
-            {
-                RaidLogs = reportsJSON
-                    .Where(x => Bosses.GetWingForBoss(x.EVTC.BossId) > 0)
-                    .Select(x => new { LogData = x, RaidWing = Bosses.GetWingForBoss(x.EVTC.BossId) })
-                    .OrderBy(x => Bosses.GetWingForBoss(x.LogData.EVTC.BossId))
-                    .ThenBy(x => Bosses.GetBossOrder(x.LogData.Encounter.BossId))
-                    .ThenBy(x => x.LogData.UploadTime)
-                    .ToList();
-            }
-            var FractalLogs = reportsJSON
-                .Where(x => allBosses
-                    .Where(y => y.Value.BossId.Equals(x.EVTC.BossId))
-                    .Where(y => y.Value.Type.Equals(BossType.Fractal))
-                    .Count() > 0)
-                .ToList();
-            var StrikeLogs = reportsJSON
-                .Where(x => allBosses
-                    .Where(y => y.Value.BossId.Equals(x.EVTC.BossId))
-                    .Where(y => y.Value.Type.Equals(BossType.Strike))
-                    .Count() > 0)
-                .ToList();
-            var GolemLogs = reportsJSON
-                .Where(x => allBosses
-                    .Where(y => y.Value.BossId.Equals(x.EVTC.BossId))
-                    .Where(y => y.Value.Type.Equals(BossType.Golem))
-                    .Count() > 0)
-                .ToList();
-            var WvWLogs = reportsJSON
-                .Where(x => allBosses
-                    .Where(y => y.Value.BossId.Equals(x.EVTC.BossId))
-                    .Where(y => y.Value.Type.Equals(BossType.WvW))
-                    .Count() > 0)
-                .ToList();
-            var OtherLogs = reportsJSON
-                .Where(x => allBosses
-                    .Where(y => y.Value.BossId.Equals(x.EVTC.BossId))
-                    .Where(y => y.Value.Type.Equals(BossType.None))
-                    .Count() > 0 || allBosses
-                    .Where(y => y.Value.BossId.Equals(x.EVTC.BossId))
-                    .Count() == 0)
-                .ToList();
-            var builder = new StringBuilder($"Session duration: {logSessionSettings.ElapsedTime}\n\n");
-            int messageCount = 0;
-            if (RaidLogs.Count > 0)
-            {
-                builder.Append("***Raid logs:***\n");
-                if (logSessionSettings.SortBy.Equals(LogSessionSortBy.UploadTime))
-                {
-                    foreach (var data in RaidLogs)
-                    {
-                        string bossName = data.LogData.Encounter.Boss + (data.LogData.ChallengeMode ? " CM" : "");
-                        var bossData = Bosses.GetBossDataFromId(data.LogData.Encounter.BossId);
-                        if (bossData != null)
-                        {
-                            bossName = bossData.Name + (data.LogData.ChallengeMode ? " CM" : "");
-                        }
-                        string duration = (data.LogData.ExtraJSON == null) ? "" : $" {data.LogData.ExtraJSON.Duration}";
-                        string successText = (logSessionSettings.ShowSuccess) ? ((data.LogData.Encounter.Success ?? false) ? " :white_check_mark:" : " ❌") : "";
-                        builder.Append($"[{bossName}]({data.LogData.Permalink}){duration}{successText}\n");
-                        if (builder.Length >= maxAllowedMessageSize)
-                        {
-                            messageCount++;
-                            discordEmbedsSuccessFailure.Add(SessionTextConstructor.MakeEmbedFromText(logSessionSettings.Name + ((messageCount > 1) ? $" part {messageCount}" : ""), builder.ToString()));
-                            builder.Clear();
-                            builder.Append("***Raid logs:***\n");
-                        }
-                    }
-                }
-                else
-                {
-                    int lastWing = 0;
-                    foreach (var data in RaidLogs)
-                    {
-                        if (!lastWing.Equals(Bosses.GetWingForBoss(data.LogData.EVTC.BossId)))
-                        {
-                            builder.Append($"**{Bosses.GetWingName(data.RaidWing)} (wing {data.RaidWing})**\n");
-                            lastWing = Bosses.GetWingForBoss(data.LogData.EVTC.BossId);
-                        }
-                        string bossName = data.LogData.Encounter.Boss + (data.LogData.ChallengeMode ? " CM" : "");
-                        var bossData = Bosses.GetBossDataFromId(data.LogData.Encounter.BossId);
-                        if (bossData != null)
-                        {
-                            bossName = bossData.Name + (data.LogData.ChallengeMode ? " CM" : "");
-                        }
-                        string duration = (data.LogData.ExtraJSON == null) ? "" : $" {data.LogData.ExtraJSON.Duration}";
-                        string successText = (logSessionSettings.ShowSuccess) ? ((data.LogData.Encounter.Success ?? false) ? " :white_check_mark:" : " ❌") : "";
-                        builder.Append($"[{bossName}]({data.LogData.Permalink}){duration}{successText}\n");
-                        if (builder.Length >= maxAllowedMessageSize)
-                        {
-                            messageCount++;
-                            discordEmbedsSuccessFailure.Add(SessionTextConstructor.MakeEmbedFromText(logSessionSettings.Name + ((messageCount > 1) ? $" part {messageCount}" : ""), builder.ToString()));
-                            builder.Clear();
-                            builder.Append($"**{Bosses.GetWingName(data.RaidWing)} (wing {data.RaidWing})**\n");
-                        }
-                    }
-                }
-            }
-            if (FractalLogs.Count > 0)
-            {
-                if (!builder.ToString().EndsWith("***\n"))
-                {
-                    builder.Append("\n\n");
-                }
-                builder.Append("***Fractal logs:***\n");
-                foreach (var log in FractalLogs)
-                {
-                    string bossName = log.Encounter.Boss;
-                    var bossData = Bosses.GetBossDataFromId(log.Encounter.BossId);
-                    if (bossData != null)
-                    {
-                        bossName = bossData.Name + (log.ChallengeMode ? " CM" : "");
-                    }
-                    string duration = (log.ExtraJSON == null) ? "" : $" {log.ExtraJSON.Duration}";
-                    string successText = (logSessionSettings.ShowSuccess) ? ((log.Encounter.Success ?? false) ? " :white_check_mark:" : " ❌") : "";
-                    builder.Append($"[{bossName}]({log.Permalink}){duration}{successText}\n");
-                    if (builder.Length >= maxAllowedMessageSize)
-                    {
-                        messageCount++;
-                        discordEmbedsSuccessFailure.Add(SessionTextConstructor.MakeEmbedFromText(logSessionSettings.Name + ((messageCount > 1) ? $" part {messageCount}" : ""), builder.ToString()));
-                        builder.Clear();
-                        builder.Append("***Fractal logs:***\n");
-                    }
-                }
-            }
-            if (StrikeLogs.Count > 0)
-            {
-                if (!builder.ToString().EndsWith("***\n"))
-                {
-                    builder.Append("\n\n");
-                }
-                builder.Append("***Strike mission logs:***\n");
-                foreach (var log in StrikeLogs)
-                {
-                    string bossName = log.Encounter.Boss;
-                    var bossData = Bosses.GetBossDataFromId(log.Encounter.BossId);
-                    if (bossData != null)
-                    {
-                        bossName = bossData.Name;
-                    }
-                    string duration = (log.ExtraJSON == null) ? "" : $" {log.ExtraJSON.Duration}";
-                    string successText = (logSessionSettings.ShowSuccess) ? ((log.Encounter.Success ?? false) ? " :white_check_mark:" : " ❌") : "";
-                    builder.Append($"[{bossName}]({log.Permalink}){duration}{successText}\n");
-                    if (builder.Length >= maxAllowedMessageSize)
-                    {
-                        messageCount++;
-                        discordEmbedsSuccessFailure.Add(SessionTextConstructor.MakeEmbedFromText(logSessionSettings.Name + ((messageCount > 1) ? $" part {messageCount}" : ""), builder.ToString()));
-                        builder.Clear();
-                        builder.Append("***Strike mission logs:***\n");
-                    }
-                }
-            }
-            if (GolemLogs.Count > 0)
-            {
-                if (!builder.ToString().EndsWith("***\n"))
-                {
-                    builder.Append("\n\n");
-                }
-                builder.Append("***Golem logs:***\n");
-                foreach (var log in GolemLogs)
-                {
-                    builder.Append($"{log.Permalink}\n");
-                    if (builder.Length >= maxAllowedMessageSize)
-                    {
-                        messageCount++;
-                        discordEmbedsSuccessFailure.Add(SessionTextConstructor.MakeEmbedFromText(logSessionSettings.Name + ((messageCount > 1) ? $" part {messageCount}" : ""), builder.ToString()));
-                        builder.Clear();
-                        builder.Append("***Golem logs:***\n");
-                    }
-                }
-            }
-            if (WvWLogs.Count > 0)
-            {
-                if (!builder.ToString().EndsWith("***\n"))
-                {
-                    builder.Append("\n\n");
-                }
-                builder.Append("***WvW logs:***\n");
-                foreach(var log in WvWLogs)
-                {
-                    builder.Append($"{log.Permalink}\n");
-                    if (builder.Length >= maxAllowedMessageSize)
-                    {
-                        messageCount++;
-                        discordEmbedsSuccessFailure.Add(SessionTextConstructor.MakeEmbedFromText(logSessionSettings.Name + ((messageCount > 1) ? $" part {messageCount}" : ""), builder.ToString()));
-                        builder.Clear();
-                        builder.Append("***WvW logs:***\n");
-                    }
-                }
-            }
-            if (OtherLogs.Count > 0)
-            {
-                if (!builder.ToString().EndsWith("***\n"))
-                {
-                    builder.Append("\n\n");
-                }
-                builder.Append("***Other logs:***\n");
-                foreach (var log in OtherLogs)
-                {
-                    string bossName = log.Encounter.Boss;
-                    var bossData = Bosses.GetBossDataFromId(log.Encounter.BossId);
-                    if (bossData != null)
-                    {
-                        bossName = bossData.Name;
-                    }
-                    string duration = (log.ExtraJSON == null) ? "" : $" {log.ExtraJSON.Duration}";
-                    string successText = (logSessionSettings.ShowSuccess) ? ((log.Encounter.Success ?? false) ? " :white_check_mark:" : " ❌") : "";
-                    builder.Append($"[{bossName}]({log.Permalink}){duration}{successText}\n");
-                    if (builder.Length >= maxAllowedMessageSize)
-                    {
-                        messageCount++;
-                        discordEmbedsSuccessFailure.Add(SessionTextConstructor.MakeEmbedFromText(logSessionSettings.Name + ((messageCount > 1) ? $" part {messageCount}" : ""), builder.ToString()));
-                        builder.Clear();
-                        builder.Append("***Other logs:***\n");
-                    }
-                }
-            }
-            if (!builder.ToString().EndsWith("***\n"))
-            {
-                messageCount++;
-                discordEmbedsSuccessFailure.Add(SessionTextConstructor.MakeEmbedFromText(logSessionSettings.Name + ((messageCount > 1) ? $" part {messageCount}" : ""), builder.ToString()));
-            }
+            var discordEmbeds = SessionTextConstructor.ConstructSessionEmbeds(reportsJSON, logSessionSettings);
             if (logSessionSettings.UseSelectedWebhooksInstead)
             {
-                await SendDiscordMessageToSelectedWebhooksAsync(logSessionSettings.SelectedWebhooks, discordEmbedsSuccessFailure, logSessionSettings.ContentText);
+                await SendDiscordMessageToSelectedWebhooksAsync(logSessionSettings.SelectedWebhooks, discordEmbeds, logSessionSettings.ContentText);
             }
             else
             {
-                await SendDiscordMessageToAllActiveWebhooksAsync(discordEmbedsSuccessFailure, logSessionSettings.ContentText);
+                await SendDiscordMessageToAllActiveWebhooksAsync(discordEmbeds, logSessionSettings.ContentText);
             }
             if (logSessionSettings.UseSelectedWebhooksInstead && logSessionSettings.SelectedWebhooks.Count > 0)
             {
@@ -416,16 +185,25 @@ namespace PlenBotLogUploader
             }
         }
 
-        private async Task SendDiscordMessageToAllActiveWebhooksAsync(List<DiscordAPIJSONContentEmbed> embeds, string contentText)
+        private async Task SendDiscordMessageToAllActiveWebhooksAsync(SessionTextConstructor.DiscordEmbeds discordEmbeds, string contentText)
         {
-            var discordContent = new DiscordAPIJSONContent()
+            string jsonContentSuccessFailure = JsonConvert.SerializeObject(new DiscordAPIJSONContent()
             {
                 Content = contentText,
-                Embeds = embeds
-            };
+                Embeds = discordEmbeds.SuccessFailure
+            });
+            string jsonContentSuccess = JsonConvert.SerializeObject(new DiscordAPIJSONContent()
+            {
+                Content = contentText,
+                Embeds = discordEmbeds.Success
+            });
+            string jsonContentFailure = JsonConvert.SerializeObject(new DiscordAPIJSONContent()
+            {
+                Content = contentText,
+                Embeds = discordEmbeds.Failure
+            });
             try
             {
-                string jsonContent = JsonConvert.SerializeObject(discordContent);
                 foreach (var key in allWebhooks.Keys)
                 {
                     var webhook = allWebhooks[key];
@@ -433,6 +211,9 @@ namespace PlenBotLogUploader
                     {
                         continue;
                     }
+                    var jsonContent =
+                        (webhook.SuccessFailToggle.Equals(DiscordWebhookDataSuccessToggle.OnSuccessAndFailure)) ? jsonContentSuccessFailure :
+                        ((webhook.SuccessFailToggle.Equals(DiscordWebhookDataSuccessToggle.OnSuccessOnly) ? jsonContentSuccess : jsonContentFailure));
                     var uri = new Uri(webhook.URL);
                     using (var content = new StringContent(jsonContent, Encoding.UTF8, "application/json"))
                     {
@@ -446,18 +227,30 @@ namespace PlenBotLogUploader
             }
         }
 
-        private async Task SendDiscordMessageToSelectedWebhooksAsync(List<DiscordWebhookData> webhooks, List<DiscordAPIJSONContentEmbed> embeds, string contentText)
+        private async Task SendDiscordMessageToSelectedWebhooksAsync(List<DiscordWebhookData> webhooks, SessionTextConstructor.DiscordEmbeds discordEmbeds, string contentText)
         {
-            var discordContent = new DiscordAPIJSONContent()
+            string jsonContentSuccessFailure = JsonConvert.SerializeObject(new DiscordAPIJSONContent()
             {
                 Content = contentText,
-                Embeds = embeds
-            };
+                Embeds = discordEmbeds.SuccessFailure
+            });
+            string jsonContentSuccess = JsonConvert.SerializeObject(new DiscordAPIJSONContent()
+            {
+                Content = contentText,
+                Embeds = discordEmbeds.Success
+            });
+            string jsonContentFailure = JsonConvert.SerializeObject(new DiscordAPIJSONContent()
+            {
+                Content = contentText,
+                Embeds = discordEmbeds.Failure
+            });
             try
             {
-                string jsonContent = JsonConvert.SerializeObject(discordContent);
                 foreach (var webhook in webhooks)
                 {
+                    var jsonContent =
+                        (webhook.SuccessFailToggle.Equals(DiscordWebhookDataSuccessToggle.OnSuccessAndFailure)) ? jsonContentSuccessFailure :
+                        ((webhook.SuccessFailToggle.Equals(DiscordWebhookDataSuccessToggle.OnSuccessOnly) ? jsonContentSuccess : jsonContentFailure));
                     var uri = new Uri(webhook.URL);
                     using (var content = new StringContent(jsonContent, Encoding.UTF8, "application/json"))
                     {
