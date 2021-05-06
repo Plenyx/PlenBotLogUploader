fb5dc4e251636ae5ced9f6d6ab4672e30e4878e3
Plenyx <1507236+Plenyx@users.noreply.github.com>
2021-05-07 00:47:00 +0200

    Added table text formatter

--- ILMerge_release.bat
+++ ILMerge_release.bat
@@ -1,1 +1,1 @@
-packages\ILMerge.3.0.41\tools\net452\ILMerge.exe /target:winexe /out:PlenBotLogUploader.exe bin\x64\Release\PlenBotLogUploader.exe bin\x64\Release\Newtonsoft.Json.dll bin\x64\Release\TwitchIrcClient.dll
+packages\ILMerge.3.0.41\tools\net452\ILMerge.exe /target:winexe /out:PlenBotLogUploader.exe bin\x64\Release\PlenBotLogUploader.exe bin\x64\Release\Newtonsoft.Json.dll bin\x64\Release\TwitchIrcClient.dll bin\x64\Release\TextTableFormatter.dll
