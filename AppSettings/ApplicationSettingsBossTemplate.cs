c08c26be7edbebb41d59dc88fc063bc42e3bc75a
Plenyx <1507236+Plenyx@users.noreply.github.com>
2021-07-20 13:39:54 +0200

    Added default text for fail & success text for Twitch responses of bosses

--- AppSettings/ApplicationSettingsBossTemplate.cs
+++ AppSettings/ApplicationSettingsBossTemplate.cs
@@ -5,9 +5,9 @@ namespace PlenBotLogUploader.AppSettings
     public class ApplicationSettingsBossTemplate
     {
         [JsonProperty("failText")]
-        public string FailText { get; set; } = "";
+        public string FailText { get; set; } = "<boss> pull: <log> | Current wipes: <pulls>";
 
         [JsonProperty("successText")]
-        public string SuccessText { get; set; } = "";
+        public string SuccessText { get; set; } = "<boss> kill: <log>";
     }
 }
