52329bc160b50e9c5949b0ca4e5eaf6745b0fbce
Plenyx <1507236+Plenyx@users.noreply.github.com>
2022-05-07 16:28:05 +0200

    Reorder the limiter type stored values

--- Teams/TeamLimiter.cs
+++ Teams/TeamLimiter.cs
@@ -2,11 +2,11 @@
 {
     public enum TeamLimiter
     {
-        AtLeast = 0,
-        Exact = 1,
-        Except = 2,
-        CommanderName = 3,
-        AND = 4,
-        OR = 5,
+        AND = 0,
+        OR = 1,
+        AtLeast = 2,
+        Exact = 3,
+        Except = 4,
+        CommanderName = 5,
     }
 }
