71b3e612d6d4bb1798b7ad6fc4584d94f6b5f3c3
Plenyx <1507236+Plenyx@users.noreply.github.com>
2022-06-22 10:52:41 +0200

    Do not use invalid characters in session filenames
    Ref. #16

--- Forms/FormLogSession.cs
+++ Forms/FormLogSession.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace PlenBotLogUploader
@@ -66,6 +67,9 @@ namespace PlenBotLogUploader
                     SelectedWebhooks = ConvertCheckboxListToList()
                 };
                 var sessionNameFormatted = textBoxSessionName.Text.ToLower().Replace(" ", string.Empty);
+                var invalidCharacters = Path.GetInvalidFileNameChars().Where(x => !x.Equals('/')).ToList();
+                invalidCharacters.ForEach(x => sessionNameFormatted = sessionNameFormatted.Replace(x.ToString(), ""));
+                sessionNameFormatted = sessionNameFormatted.Replace(@"/", "-out-of-");
                 var fileName = $"{((!string.IsNullOrWhiteSpace(sessionNameFormatted)) ? $"{sessionNameFormatted} " : "")}{sessionTimeStarted.Year}-{sessionTimeStarted.Month}-{sessionTimeStarted.Day} {sessionTimeStarted.Hour}-{sessionTimeStarted.Minute}-{sessionTimeStarted.Second}";
                 File.AppendAllText($"{ApplicationSettings.LocalDir}{fileName}.csv", "Boss;BossId;Success;Duration;RecordedBy;EliteInsightsVersion;arcdpsVersion;Permalink\n");
                 foreach (var reportJSON in mainLink.SessionLogs)
