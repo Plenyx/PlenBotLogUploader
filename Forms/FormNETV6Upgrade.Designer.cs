750e4b687a5e35cbd1410be9ff904d224bb1becb
Plenyx <1507236+Plenyx@users.noreply.github.com>
2022-08-24 12:13:00 +0200

    Update the NETV6 link to the latest release

--- Forms/FormNETV6Upgrade.Designer.cs
+++ Forms/FormNETV6Upgrade.Designer.cs
@@ -36,9 +36,10 @@
             // buttonAcknowledge
             // 
             this.buttonAcknowledge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.buttonAcknowledge.Location = new System.Drawing.Point(286, 444);
+            this.buttonAcknowledge.Location = new System.Drawing.Point(214, 361);
+            this.buttonAcknowledge.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonAcknowledge.Name = "buttonAcknowledge";
-            this.buttonAcknowledge.Size = new System.Drawing.Size(359, 39);
+            this.buttonAcknowledge.Size = new System.Drawing.Size(269, 32);
             this.buttonAcknowledge.TabIndex = 0;
             this.buttonAcknowledge.Text = "I have .NET 6.0 installed, update now";
             this.buttonAcknowledge.UseVisualStyleBackColor = true;
@@ -48,23 +49,24 @@
             // 
             this.richTextBoxUpgradeInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBoxUpgradeInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.richTextBoxUpgradeInfo.Location = new System.Drawing.Point(12, 12);
+            this.richTextBoxUpgradeInfo.Location = new System.Drawing.Point(9, 10);
+            this.richTextBoxUpgradeInfo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.richTextBoxUpgradeInfo.Name = "richTextBoxUpgradeInfo";
-            this.richTextBoxUpgradeInfo.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBoxUpgradeInfo.Size = new System.Drawing.Size(879, 426);
+            this.richTextBoxUpgradeInfo.Size = new System.Drawing.Size(659, 346);
             this.richTextBoxUpgradeInfo.TabIndex = 1;
             this.richTextBoxUpgradeInfo.Text = resources.GetString("richTextBoxUpgradeInfo.Text");
             this.richTextBoxUpgradeInfo.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.RichTextBoxUpgradeInfo_LinkClicked);
             // 
             // FormNETV6Upgrade
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(903, 491);
+            this.ClientSize = new System.Drawing.Size(677, 399);
             this.Controls.Add(this.richTextBoxUpgradeInfo);
             this.Controls.Add(this.buttonAcknowledge);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormNETV6Upgrade";
