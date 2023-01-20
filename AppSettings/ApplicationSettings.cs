0ddd51a0526f5428aa00a0c736b09016736514f9
Plenyx <1507236+Plenyx@users.noreply.github.com>
2023-01-20 23:45:48 +0100

    Release 81

--- AppSettings/ApplicationSettings.cs
+++ AppSettings/ApplicationSettings.cs
@@ -9,7 +9,7 @@ namespace PlenBotLogUploader.AppSettings
     internal sealed class ApplicationSettings
     {
         #region application version
-        internal static int Version => 80;
+        internal static int Version => 81;
         #endregion
 
         #region load & save functionality
