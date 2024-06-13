a6849646d68fc6cff5e022dd352d88c7a4dd9dbe
Plenyx <1507236+Plenyx@users.noreply.github.com>
2024-06-13 10:13:23 +0200

    Added a check for error 403 from dps.report

--- Forms/FormMain.cs
+++ Forms/FormMain.cs
@@ -779,7 +779,18 @@ namespace PlenBotLogUploader
                             timerFailedLogsReupload.Start();
                             return;
                         }
-                        AddToText($">:> Unable to upload file {Path.GetFileName(file)}, dps.report responded with an non-ok status code ({(int)responseMessage.StatusCode})");
+                        if (statusCode == 403)
+                        {
+                            // remove from failed logs if present
+                            var removed = LogReuploader.FailedLogs.Remove(file);
+                            if (removed)
+                            {
+                                LogReuploader.SaveFailedLogs();
+                            }
+                            AddToText($">:> Ignoring file {Path.GetFileName(file)}, dps.report is unable to process it.");
+                            return;
+                        }
+                        AddToText($">:> Unable to upload file {Path.GetFileName(file)}, dps.report responded with an non-ok status code ({(int)responseMessage.StatusCode}).");
                         return;
                     }
                     var response = await responseMessage.Content.ReadAsStringAsync();
